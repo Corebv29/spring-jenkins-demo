16ce3e7d3bd78652311409f115333ffa3b822329
Bryan <=>
2022-09-21 16:51:26 -0600

    second commit

--- src/test/java/com/corebv/jenkins/api/SpringJenkinsDemoApplicationTests.java
+++ src/test/java/com/corebv/jenkins/api/SpringJenkinsDemoApplicationTests.java
@@ -15,6 +15,7 @@ class SpringJenkinsDemoApplicationTests {
     @Test
     void contextLoads() {
         logger.info("Test case executing");
+        logger.info("Test case executing second log");
         assertEquals(true,true);
     }
 
