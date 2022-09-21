6a4a198389ed2cc0fa644a7c54f63369b27f2d88
Bryan <=>
2022-09-21 16:19:08 -0600

    first commit

---
+++ src/test/java/com/corebv/jenkins/api/SpringJenkinsDemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.corebv.jenkins.api;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringJenkinsDemoApplicationTests {
+
+    public static Logger logger = LoggerFactory.getLogger(SpringJenkinsDemoApplicationTests.class);
+    
+    @Test
+    void contextLoads() {
+        logger.info("Test case executing");
+        assertEquals(true,true);
+    }
+
+}
